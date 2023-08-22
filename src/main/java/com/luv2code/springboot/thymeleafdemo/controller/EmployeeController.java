0dacf4f58819b13a53d6dbbb0f268bd358d22522
Karol <karolkostecki1@gmail.com>
2023-08-22 18:34:31 +0200

    Add Update and Delete

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -2,13 +2,10 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -30,15 +27,48 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		//	create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		//	get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		//	set employee in the model to prepopulate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		//	send over to out form
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+		// save
+		employeeService.save(theEmployee);
+
+		//	use a redirect to prevent duplicated submissions
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+
+		// delete
+		employeeService.deleteById(theId);
+
+		//	use a redirect to prevent duplicated submissions
+		return "redirect:/employees/list";
 	}
 }
-
-
-
-
-
-
-
-
-
